50405acf3167065ebc9e14ad467803806394f802
PavloWick <pavlopuzik@gmail.com>
2024-03-14 16:25:43 -0700

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
   useEffect(() => {
     if (authUser) {
       // Our real servers port number 5000
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://wick-chat.onrender.com", {
         query: {
           userId: authUser._id,
         },
