1113bc3f176948bf6d4578d818117bb133ea750b
PavloWick <pavlopuzik@gmail.com>
2024-03-15 16:41:01 -0700

    Fixed user controler

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,16 +1,19 @@
 import User from "../models/user.model.js";
 
-export const getUsersForSidebar = async(req,res) => {
-    try {
-
-        const loggedInUserId = req.user._id;
-        // find everyone except loggedInUserId, thats what the $ne is for
-        const filteredUsers = await User.find({_id: { $ne: loggedInUserId }}).select("-password");
-        
-        res.status(200).json(filteredUsers);
-
-    } catch(error) {
-        process.stdout.write("Error in getUsersForSidebar: ", error.message);
-      res.status(500).json({error: 'Internal server error'});
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+    if (!loggedInUserId) {
+      throw new Error(error);
     }
-}
+    // find everyone except loggedInUserId, thats what the $ne is for
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
+
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    process.stdout.write(`Error in getUsersForSidebar: ${error.message}`);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
